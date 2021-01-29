7b3cb0d3d3e569f24fb17006406c24290c25d0f7
Daniel Guimarães <44902038+danguimaraes86@users.noreply.github.com>
2021-01-29 01:39:48 -0300

    add: small fixes and dbPokemonNames

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,12 +2,12 @@ import Head from 'next/head'
 
 export default function Layout({ children }) {
   return (
-    <div className='bg-gray vh-100'>
+    <div className='bg-gray'>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1' crossOrigin='anonymous' />
-        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css' />
+        <script src="https://kit.fontawesome.com/1500b09f5a.js" crossOrigin="anonymous"></script>
         <link rel='icon' href='/favicon.ico' />
         <title>PokeTrade</title>
       </Head>
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,16 +1,18 @@
 
 export default function NavBar() {
   return (
-    <nav className='navbar navbar-dark bg-dark border-bottom border-warning'>
-      <div className='container-fluid'>
-        <div className='navbar-brand'>
-          <img src='/favicon.ico' width='24' height='24' className='navbar-brand' />
+      <nav className='navbar navbar-dark bg-dark border-bottom border-warning'>
+        <div className='container'>
+          <div className='d-flex navbar-brand'>
+            <img src='/pokeapi_256.png' width='60' height='48' className='navbar-brand' />
+            <div className='align-self-center text-warning'>
               PokeTrade
             </div>
-        <div>
-          <i className='bi bi-clock-fill' style={{ color: 'white', fontSize: '1.5em' }} />
+          </div>
+          <div>
+            <i className="fas fa-history text-warning text-center my-auto fs-3" aria-hidden />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
   )
 }
